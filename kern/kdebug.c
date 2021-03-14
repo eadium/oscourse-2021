be954c5f54c23723df15c0b5dffe906bf70e544d
Artem Andryukhov <akandryukhov@edu.hse.ru>
2021-03-14 21:29:12 +0300

    working lab 2

--- kern/kdebug.c
+++ kern/kdebug.c
@@ -66,6 +66,12 @@ debuginfo_rip(uintptr_t addr, struct Ripdebuginfo *info) {
     * Hint: use line_for_address from kern/dwarf_lines.c */
 
     // LAB 2: Your res here:
+    void *buf;
+    buf  = &tmp_buf;
+    addr -= 5;
+    buf  = &info->rip_line;
+    res = line_for_address(&addrs, addr, line_offset, buf);
+    if (res < 0) goto error;
 
     /* Find function name corresponding to given address.
     * Hint: note that we need the address of `call` instruction, but rip holds
@@ -75,7 +81,12 @@ debuginfo_rip(uintptr_t addr, struct Ripdebuginfo *info) {
     * string returned by function_by_info will always be */
 
     // LAB 2: Your res here:
-
+    buf  = &tmp_buf;
+    res = function_by_info(&addrs, addr, offset, buf, &info->rip_fn_addr);
+    // cprintf("buf %d ", (int)info->rip_fn_addr);
+    if (res < 0) goto error;
+    strncpy(info->rip_fn_name, tmp_buf, 256);
+    info->rip_fn_namelen = strnlen(info->rip_fn_name, 256);
 error:
     return res;
 }
